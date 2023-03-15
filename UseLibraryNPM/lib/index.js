334005ae08b4a5e4bd77f62e88399a3c169da83a
vmunozvue <victor.munoz@col.vueling.com>
2023-03-15 22:09:13 +0100

    Using of a npmjs library

---
+++ UseLibraryNPM/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var calculator_1 = require("prueba-libreria-npm/lib/calculator");
+var myCalculator = new calculator_1.Calculator();
+console.log(myCalculator.add(5, 5));
---
+++ UseLibraryNPM/src/index.ts
@@ -0,0 +1,3 @@
+import { Calculator } from 'prueba-libreria-npm/lib/calculator'
+let myCalculator = new Calculator()
+console.log(myCalculator.add(5,5))
